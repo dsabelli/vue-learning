c60538f13b8f03dd01632199fd58ee7c25b8f759
dsabelli <dsabelli3@gmail.com>
2023-02-27 10:57:35 -0500

    reinitialize vue router

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
+import router from "./router";
 
 import "./assets/main.css";
 import "./index.css";
 
 const app = createApp(App);
 
+app.use(router);
 app.use(createPinia());
 
 app.mount("#app");
